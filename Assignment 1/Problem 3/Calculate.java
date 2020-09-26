fe10d03eea782dc3e1748317bd5c06a9948be6e4
Ravi Shankar <ravishankar98910@gmail.com>
2020-09-26 14:53:26 +0530

    Timer And KeyEvent Handler Added

--- Assignment 1/Problem 3/Calculate.java
+++ Assignment 1/Problem 3/Calculate.java
@@ -1,9 +1,43 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.KeyboardFocusManager;
+import java.awt.KeyEventDispatcher;
+
 // Calculate class to handle 'ENTER' and 'SPACE' functionalites.
 public class Calculate {
+	private final static int ONE_SECOND = 1000;
+	private static char keyPressed = 'a';
 	// Main function.
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		Thread thread = new Thread(calculator);
-		thread.start();
+		ActionListener ticktock = new ActionListener() {
+			public void actionPerformed(ActionEvent evt){
+				if(keyPressed == '\n'){
+					System.out.println("Enter");
+				}
+				else if(keyPressed == ' '){
+					System.out.println("Space");
+				}
+				keyPressed = 'a';
+				calculator.button[calculator.highlighted_Button].setBackground(Color.WHITE);
+				calculator.highlighted_Button += 1;
+				calculator.highlighted_Button %= 15;
+				calculator.button[calculator.highlighted_Button].setBackground(Color.RED);
+			}
+		};
+		Timer timer = new Timer(ONE_SECOND, ticktock);
+		timer.start();
+
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+			@Override
+			public boolean dispatchKeyEvent(KeyEvent e){
+				keyPressed = e.getKeyChar();
+				return false;
+			}
+		});
+
+
+
 	}
 }
--- Assignment 1/Problem 3/Calculator.java
+++ Assignment 1/Problem 3/Calculator.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.Color;
 
-public class Calculator implements Runnable {
+public class Calculator {
+	private final static int ONE_SECOND = 1500;
 	JFrame frame;
-	JButton button[];
+	JButton button[] = new JButton[15];   // Set of buttons for each number and operations. 
 	JLabel screen;
+	Integer highlighted_Button = 0;
 
 	// Constructor
 	public Calculator(){
@@ -15,12 +18,11 @@ public class Calculator implements Runnable {
 	    screen.setText("Enter Some Numbers...");
 	    screen.setBounds(90, 100, 300, 40);
 	    frame.add(screen);
-
-	    JButton button[] = new JButton[15]; // Set of buttons for each number and operations. 
 	    
 	    for(Integer i=1;i<10;i++){
 	      button[i] = new JButton(i.toString());
 	      button[i].setBounds(80 +((i-1)%3)*50, 150 + ((i-1)/3)*50, 50, 50);
+	      button[i].setBackground(Color.WHITE);
 	      frame.add(button[i]);
 	    }
 
@@ -29,30 +31,23 @@ public class Calculator implements Runnable {
 	    for(Integer i=0;i<4;i++){
 	      button[i+10] = new JButton(""+operation.charAt(i));
 	      button[i+10].setBounds(240 +(i%2)*50, 150 + (i/2)*50, 50, 50);
+	      button[i+10].setBackground(Color.WHITE);
 	      frame.add(button[i+10]);
 	    }
 
 	    button[0] = new JButton("0");
 	    button[0].setBounds(240, 250, 100, 50);
+	    button[0].setBackground(Color.WHITE);
 	    frame.add(button[0]);
 
-	    button[14] = new JButton("Calculate When Highlighted");
+	    button[14] = new JButton("Press Enter To Calculate");  // Result calculation button.
 	    button[14].setBounds(80, 310, 260, 50);
+	    button[14].setBackground(Color.WHITE);
 	    frame.add(button[14]);
-
-
-
-
 
 	    frame.setSize(500, 500);
 	    frame.setLayout(null);
 	    frame.setVisible(true);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
-	@Override
-	public void run() {
-		return;
-	}
-
 }
