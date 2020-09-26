96edcf2e85d5ff45cb84ae778380482fe83e00ac
Ravi Shankar <ravishankar98910@gmail.com>
2020-09-26 13:07:01 +0530

    GUI changes

--- Assignment 1/Problem 3/Calculator.java
+++ Assignment 1/Problem 3/Calculator.java
@@ -5,54 +5,49 @@ public class Calculator implements Runnable {
 	JFrame frame;
 	JButton button[];
 	JLabel screen;
-	JTextField input;
-	JButton exit;
-	boolean exitNow = false;
 
-    // x axis, y axis, width, height
+	// Constructor
 	public Calculator(){
-	    frame = new JFrame();
+		// Designing the calculator GUI.
+	    frame = new JFrame("Calculator Is Ready To Compute");
 
-	    screen = new JLabel();
+	    screen = new JLabel();    // Result output screen. 
 	    screen.setText("Enter Some Numbers...");
 	    screen.setBounds(90, 100, 300, 40);
 	    frame.add(screen);
 
-	    JButton button[] = new JButton[14];
-	    for(Integer i=0;i<10;i++){
+	    JButton button[] = new JButton[15]; // Set of buttons for each number and operations. 
+	    
+	    for(Integer i=1;i<10;i++){
 	      button[i] = new JButton(i.toString());
-	      button[i].setBounds(90 +(i%5)*50, 150 + (i/5)*50, 50, 50);
+	      button[i].setBounds(80 +((i-1)%3)*50, 150 + ((i-1)/3)*50, 50, 50);
 	      frame.add(button[i]);
 	    }
 
-	    String operation = "+-*/";
+	    String operation = "+-*/";          // String of all operations that can be performed.
 
 	    for(Integer i=0;i<4;i++){
 	      button[i+10] = new JButton(""+operation.charAt(i));
-	      button[i+10].setBounds(90 +(i%5)*50, 250 + (i/5)*50, 50, 50);
+	      button[i+10].setBounds(240 +(i%2)*50, 150 + (i/2)*50, 50, 50);
 	      frame.add(button[i+10]);
 	    }
-	    
-	    exit = new JButton("Exit");
-	    exit.setBounds(90, 310, 250, 50);
 
-	    exit.addActionListener(new ActionListener() {
-	      @Override
-	      public void actionPerformed(ActionEvent e) {
-	      	exitNow = true;
-	        System.out.println("Exit Calculator");
-	      }
-	    });
+	    button[0] = new JButton("0");
+	    button[0].setBounds(240, 250, 100, 50);
+	    frame.add(button[0]);
 
-	    frame.add(exit);
+	    button[14] = new JButton("Calculate When Highlighted");
+	    button[14].setBounds(80, 310, 260, 50);
+	    frame.add(button[14]);
 
-	    input = new JTextField();
-	    input.setBounds(90, 370, 250, 50);
-	    frame.add(input);
+
+
+
 
 	    frame.setSize(500, 500);
 	    frame.setLayout(null);
 	    frame.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	@Override
