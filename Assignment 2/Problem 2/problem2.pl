c3dce63fe5f91f8e1f2c9390e92d93a242b5276b
Ravi Shankar <ravishankar98910@gmail.com>
2020-10-11 00:43:51 +0530

    Problem 2 Optimal Distance Done

--- Assignment 2/Problem 2/problem2.pl
+++ Assignment 2/Problem 2/problem2.pl
@@ -6,9 +6,9 @@ bus(123,amingaon,jalukbari,14.5,15,10,10).
 bus(13,jalukbari,paltanbazar,16,18,10,10).
 bus(756,panbazar,chandmari,16,16.5,7,8).
 
-% Depth First Search function for checking if a path exists.
+% Depth First Search function for finding the path with the minimum distance.
 
-dfs(X, Path, Y) :-
+dfs_OptimalDistance(X, Path, Y, TotalDistance) :-
 	(	
 		% If the current node is not visited in path considered.
 		not(member(X, Path)) ->
@@ -16,21 +16,23 @@ dfs(X, Path, Y) :-
 			append(Path, [X], UpdatedPath),
 			(
 				(X == Y) ->
-					write("Ravi\n"),
-					atomic_list_concat(UpdatedPath, ' -> ', Atom),
-					atom_string(Atom, String),
-				    format('~w~n', String),
-				    open('output.txt', append, Stream),
-				    write(Stream, String),
-				    nl(Stream),
-				    close(Stream)
+					nb_getval(optimalDistance, OptimalDistance),
+    				UpdatedOptimalDistance is min(OptimalDistance, TotalDistance),
+				    nb_setval(optimalDistance, UpdatedOptimalDistance),
+				    (
+				        % If current path has the minimum distance value (till now), update minimum path list
+				        UpdatedOptimalDistance =:= TotalDistance -> nb_setval(optimalPath, UpdatedPath) 
+				        ; 
+				        write("")
+				    )
 				;
 					write("")
 			),
 			forall(
-				bus(_, X, DP, _, _, _, _),
+				bus(_, X, DP, _, _, Distance, _),
 				(	
-					dfs(DP, UpdatedPath, Y)
+					UpdatedDistance is TotalDistance + Distance,
+					dfs_OptimalDistance(DP, UpdatedPath, Y, UpdatedDistance)
 				)
 			)
 		;
@@ -41,7 +43,23 @@ dfs(X, Path, Y) :-
 
 route(X, Y) :-
 	(
+		nb_setval(optimalDistance, 1000000),
+    	nb_setval(optimalPath, []),
+
 		% To check if atleast a path exists between X and Y by simple Depth First Search.
-		dfs(X, [], Y)
+
+		dfs_OptimalDistance(X, [], Y, 0),
+
+		nb_getval(optimalDistance, OptimalDistance),
+		nb_getval(optimalPath, OptimalPath),
+
+		atomic_list_concat(OptimalPath, ' -> ', Atom),
+		atom_string(Atom, String),
+	    format('~w~n', String),
+	    open('output.txt', append, Stream),
+	    write(Stream, String),
+	    write("Minimum Distance To Travel : "), write(OptimalDistance), writeln("km"),
+	    nl(Stream),
+	    close(Stream)
 
 	).
