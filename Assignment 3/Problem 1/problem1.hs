52fa4d26a8c90b4b4ea7c6b81738c84485f13d2e
Ravi Shankar <ravishankar98910@gmail.com>
2020-11-09 13:30:32 +0530

    problem1 addition and duplicate removal done

--- Assignment 3/Problem 1/problem1.hs
+++ Assignment 3/Problem 1/problem1.hs
@@ -3,8 +3,15 @@ import Data.List
 import Data.Bool
 
 -- Two sets 'A' and 'B' to be used in the program.
-setA = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-setB = [5, 6, 7, 8, 9, 10, 11]
+setA = [1, 2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+setB = [5, 2, 6, 7, 8, 9, 10, 11]
+
+
+-- Function to remove duplicates from a given set.
+removeDuplicates :: (Eq a) => [a] -> [a]
+removeDuplicates [] = []
+removeDuplicates [x] = [x]
+removeDuplicates (x:xs) = x : [ k  | k <- removeDuplicates (xs), k /=x ]
 
 
 
@@ -18,12 +25,19 @@ findUnion (b:sb) sa = do -- If first set is not empty.
     else 
         b : findUnion sb sa
 
+-- Function to handle duplicate values in the union of two sets.
+findUnion' :: [Integer] -> [Integer] -> [Integer]
+findUnion' sb sa = do
+    let temp = findUnion sb sa
+    let set_union = sa ++ temp
+    removeDuplicates set_union
+
 -- Function to handle union of set and user input.
 setUnion :: Char -> Char -> IO ()
 setUnion x y | (x /= 'A' && x /= 'B') || (y /= 'B' && y /= 'A') = putStrLn "ONLY A OR B ARE SUPPORTED"
-             | x == y && x == 'A' = print (setA)
-             | x == y && x == 'B' = print (setB)
-             | x /= y = print (setA ++ findUnion setB setA)
+             | x == y && x == 'A' = print (removeDuplicates setA)
+             | x == y && x == 'B' = print (removeDuplicates setB)
+             | x /= y = print (findUnion' setB setA)
 
 
 
@@ -38,12 +52,18 @@ findIntersect (a:sa) sb = do -- If first set is not empty.
     else 
         findIntersect sa sb
 
+-- Function to handle the duplicate values in the intersection of two sets.
+findIntersect' :: [Integer] -> [Integer] -> [Integer]
+findIntersect' sa sb = do
+    let set_intersect = findIntersect sa sb
+    removeDuplicates set_intersect
+
 -- Function to handle intersection of set and user input.
 setIntersect :: Char -> Char -> IO ()
 setIntersect x y | (x /= 'A' && x /= 'B') || (y /= 'B' && y /= 'A') = putStrLn "ONLY A OR B ARE SUPPORTED"
-                 | x == y && x == 'A' = print (setA)
-                 | x == y && x == 'B' = print (setB)
-                 | x /= y = print (findIntersect setA setB)
+                 | x == y && x == 'A' = print (removeDuplicates setA)
+                 | x == y && x == 'B' = print (removeDuplicates setB)
+                 | x /= y = print (findIntersect' setA setB)
 
 
 
@@ -58,23 +78,39 @@ findDifference (a:sa) sb = do  -- If first set is not empty.
     else
         a : findDifference sa sb
 
+-- Function to handle the duplicate values in the difference of two sets.
+findDifference' :: [Integer] -> [Integer] -> [Integer]
+findDifference' sa sb = do
+    let set_difference = findDifference sa sb
+    removeDuplicates set_difference
+
 -- Function to handle difference of set and user input.
 setDifference :: Char -> Char -> IO ()
 setDifference x y | (x /= 'A' && x /= 'B') || (y /= 'B' && y /= 'A') = putStrLn "ONLY A OR B ARE SUPPORTED"
-                  | x == 'A' && y == 'A' = print (findDifference setA setA)
-                  | x == 'B' && y == 'B' = print (findDifference setB setB) 
-                  | x /= y && x == 'A' && y == 'B' = print (findDifference setA setB)
-                  | x /= y && x == 'B' && y == 'A' = print (findDifference setB setA)
+                  | x == 'A' && y == 'A' = print (findDifference' setA setA)
+                  | x == 'B' && y == 'B' = print (findDifference' setB setB) 
+                  | x /= y && x == 'A' && y == 'B' = print (findDifference' setA setB)
+                  | x /= y && x == 'B' && y == 'A' = print (findDifference' setB setA)
 
 
 
+-- Function to handle addition of two sets.
+findAdd :: [Integer] -> [Integer] -> [Integer]
+findAdd [] _ = []   -- If first set is empty.
+findAdd _ [] = []   -- If second set is empty.
+
+findAdd (a:sa) (b:sb) = do
+    let temp1 = findAdd sa (b:sb)
+    let add1 = (a+b : temp1)
+    let temp2 = findAdd (a:sa) sb
+    removeDuplicates (add1 ++ temp2)
 
 -- Function to handle addition of sets and user input.
 setAdd :: Char -> Char -> IO ()
 setAdd x y | (x /= 'A' && x /= 'B') || (y /= 'B' && y /= 'A') = putStrLn "ONLY A OR B ARE SUPPORTED"
-           | x == 'A' && y == 'A' = print (setA ++ setA)
-           | x == 'B' && y == 'B' = print (setB ++ setA)
-           | x /= y = print (setA ++ setB)
+           | x == 'A' && y == 'A' = print (findAdd setA setA)
+           | x == 'B' && y == 'B' = print (findAdd setB setB)
+           | x /= y = print (findAdd setA setB)
 
 
 
@@ -88,13 +124,14 @@ isEmpty x | (x /= 'A' && x /= 'B') = putStrLn "ONLY A OR B ARE SUPPORTED"
           | x == 'B' && length setB /= 0 = putStrLn "Set B Is Not Empty"
 
 
+
 -- Function to print all condition of operation between sets A and B.
 main = do
     putStrLn "Set A Is "
-    print (setA)
+    print (removeDuplicates setA)
     putStrLn "\n"
     putStrLn "Set B Is "
-    print (setB)
+    print (removeDuplicates setB)
     putStrLn "\n"
 
     putStrLn "Check If Set A Is Empty"
--- Assignment 3/Problem 3/problem3.hs
+++ Assignment 3/Problem 3/problem3.hs
@@ -4,3 +4,27 @@ import Data.IORef
 import Data.Maybe
 import System.Random
 import System.IO.Unsafe
+
+minKitchen :: Integer -> Integer
+minKitchen bedroom = do
+    let kitchen = bedroom `div` 3
+    let left = bedroom `mod` 3
+    if left > 0 then kitchen+1
+        else kitchen
+
+design :: Integer -> Integer -> Integer -> IO ()
+design area bedroom hall = do
+    let bathroom = bedroom + 1
+    let kitchen = minKitchen bedroom
+    let garden = 1
+    let balcony = 1
+    print ("Bedroom: " ++ (show bedroom) ++ " Hall: " ++ (show hall) ++ " Bathroom: " ++ (show bathroom) ++ " Kitchen: " ++ (show kitchen) ++ " Garden: " ++ (show garden) ++ " Balcony: " ++ (show balcony))
+
+    let minTotalArea = (bedroom * 100) + (hall * 150) + (bathroom * 20) + (kitchen * 35) + (garden * 100) + (balcony * 25)
+    print("Minimum Area Needed For Basic Arrangement: " ++ (show minTotalArea))
+
+    if area < minTotalArea then print ("Sorry! Area Is Too Small To Design")
+        else print ("Most Effective Design Is Below: ")
+
+main = do
+    design 100 5 5
